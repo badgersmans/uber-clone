1f827167220af81f064b39cbe2b964710ca4acb0
Shawn Law <shawnlaw@tuta.io>
2022-10-18 17:15:54 +0800

    4.29.23 progress on HomeScreen

---
+++ components/NavOptions.js
@@ -0,0 +1,54 @@
+import { View, Text, FlatList, TouchableOpacity, Image } from 'react-native'
+import tw from 'twrnc'
+import { Icon } from "@rneui/themed";
+
+const data = [
+    {
+        id: "123",
+        title: "Get a ride",
+        image: "https://links.papareact.com/3pn",
+        screen: "MapScreen",
+    },
+    {
+        id: "456",
+        title: "Order food",
+        image: "https://links.papareact.com/28w",
+        screen: "EatsScreen",
+    },
+];
+
+const NavOptions = () => {
+  return (
+    <FlatList 
+        data={data}
+        horizontal
+        renderItem={({ item }) => (
+            <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+               <View>
+                <Image 
+                    source={{
+                        uri: item.image
+                    }}
+                    style={{
+                        width: 120,
+                        height: 120,
+                        resizeMode: 'contain'
+                    }}
+                />
+                <Text style={tw`mt-2 text-lg font-semibold`}>{ item.title }</Text>
+                <Icon 
+                    type="antdesign"
+                    name="arrowright"
+                    color="white"
+                    style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+                />
+
+               </View>
+            </TouchableOpacity>
+        )}
+        keyExtractor={(item) => item.id}
+    />
+  )
+}
+
+export default NavOptions
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,25 @@
-import { SafeAreaView, Text, View } from 'react-native'
+import { Image, SafeAreaView, Text, View } from 'react-native'
 import tw from 'twrnc';
+import NavOptions from '../components/NavOptions';
 
 const HomeScreen = () => {
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
-      <Text style={tw`text-red-500 p-10`}>HomeScreen</Text>
+        <View style={tw`p-5`}>
+            <Image 
+                source={{
+                    uri: "https://links.papareact.com/gzs"
+                }}
+                style={{
+                    width: 100,
+                    height: 100,
+                    resizeMode: 'contain'
+                }}
+            />
+
+            <NavOptions />
+        </View>
+
     </SafeAreaView>
   )
 }
