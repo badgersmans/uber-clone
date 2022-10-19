f15e16c48a4516658f86605ccfdab8cc5db98ee6
Shawn Law <shawnlaw@tuta.io>
2022-10-19 21:08:27 +0800

    5.44.11 building NavFavourites component...

---
+++ components/NavFavourites.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const NavFavourites = () => {
+  return (
+    <View>
+      <Text>NavFavourites</Text>
+    </View>
+  )
+}
+
+export default NavFavourites
