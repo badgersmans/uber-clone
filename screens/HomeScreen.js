ba602d728d40a3e484a56f2b3fad96e46c09866e
Shawn Law <shawnlaw@tuta.io>
2022-10-19 10:38:16 +0800

    5.10.42 some progress...

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,13 @@
-import { Image, SafeAreaView, Text, View } from 'react-native'
+import { Image, SafeAreaView, Text, View } from 'react-native';
 import tw from 'twrnc';
 import NavOptions from '../components/NavOptions';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from '@env';
+import { useDispatch } from 'react-redux';
+import { setDestination, setOrigin } from '../slices/navSlice';
 
 const HomeScreen = () => {
+    const dispatch = useDispatch();
 
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
@@ -15,6 +20,37 @@ const HomeScreen = () => {
                     width: 100,
                     height: 100,
                     resizeMode: 'contain'
+                }}
+            />
+
+            <GooglePlacesAutocomplete
+                nearbyPlacesAPI='GooglePlacesSearch'
+                debounce={400}
+                placeholder='From Where?'
+                styles={{
+                    container: {
+                        flex: 0,
+                    },
+                    textInput: {
+                        fontSize: 18,
+                    }
+                }}
+                query={{
+                    key: GOOGLE_MAPS_APIKEY,
+                    language: 'en'
+                }}
+                minLength={2}
+                enablePoweredByContainer={false}
+                returnKeyType={"search"}
+                fetchDetails={true}
+                onPress={(data, details = null) => {
+                    // console.log(data);
+                    // console.log(details);
+                    dispatch(setOrigin({
+                        location: details.geometry.location,
+                        description: data.description,
+                    }))
+                    dispatch(setDestination(null))
                 }}
             />
 
